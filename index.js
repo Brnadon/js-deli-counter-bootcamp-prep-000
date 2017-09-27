021b046cdac66439790aabb016bd84a04d0284e4
Brandon Brasson <rocksong622@gmail.com>
2017-09-27 21:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 katz_deli = [];
- 
+
  def line(katz_deli)
    line_array = []
    if katz_deli.length == 0
@@ -8,11 +8,11 @@ katz_deli = [];
      katz_deli.each.with_index(1) do |name, index|
        line_array.push("#{index}. #{name}")
     }
- 
+
   def take_a_number(katz_deli, name)
    katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
- 
+
  }
  def now_serving(katz_deli)
    if katz_deli.count == 0
